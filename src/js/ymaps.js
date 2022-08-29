064b9dff20ad416d49b4350ea48efc146d17a7c3
Alfiya <al.yausheva@gmail.com>
2022-08-29 21:42:09 +0300

    initial

--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -1,12 +1,20 @@
+import { map } from "jquery";
+
 function mapInit() {
     ymaps.ready(() => {
         let city = new ymaps.Map('map', {
             center: [55.76, 37.65],
-            zoom: 9
-        })
-    })
-}
+            zoom: 12,
+            controls: ['zoomControl'],
+            behavoirs:['drag']
+        });
+        var placemark = new ymaps.Placemark([55.76, 37.30], {
+        
+    });
+})
+map.geoObjects.add(placemark);
 
+}
 export {
     mapInit
 }
