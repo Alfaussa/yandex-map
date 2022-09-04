396e4a63be4744f1c7046a5eae465cd95e7a579b
Alfiya <al.yausheva@gmail.com>
2022-09-04 13:51:16 +0300

    added functionality

--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -1,5 +1,9 @@
-// import { map } from "jquery";
-ymaps.ready(mapInit);
+const { map } = require("jquery");
+import {form} from '../templates/form'
+export {mapInit}
+document.addEventListener('DOMContentLoaded', () => ymaps.ready(mapInit));
+
+let clusterer;
 
 function mapInit() {
     ymaps.ready(() => {
@@ -9,52 +13,82 @@ function mapInit() {
             controls: ['zoomControl'],
             behavoirs:['drag']
         });
-        // function placemarkInit() {
-        //     document.addEventListener('click', () =>{
-              
-        //         var placemark = new ymaps.Placemark([55.76, 37.64], {
-        //             // hintContent:'this is hint', 
-        //             balloonContent: [
-        //               '  <div class = \'form\'>',
-        //                 '<h3>Отзыв:</h3>',
-        //                 '<input placeholder=\'Укажите ваше имя\'> </input>',
-        //                 '<input placeholder=\'Укажите ваше место\'> </input>',
-        //                 '<textarea placeholder=\'Укажите ваше место\'></textarea>',
-        //                 '</div>'
-        //             ].join('')
-        //     });
-        //     city.geoObjects.add(placemark);
-        //     })
-        // }
-        // placemarkInit();
+      
     city.events.add('click', function (e) {
+        var coords = e.get('coords');
+        
+        openBallon(city, coords, []);
 
-            if (!city.balloon.isOpen()) {
-               
-                var coords = e.get('coords');
-                
-            city.balloon.open(coords, {
-                    contentHeader:'Отзывы:',
-                    contentBody:[
-                        '<div class = \'form\'>',
-                        '<input placeholder=\'Укажите ваше имя\'> </input>',
-                        '<input placeholder=\'Укажите ваше место\'> </input>',
-                        '<textarea placeholder=\'Укажите ваше место\'></textarea>',
-                        '</div>'
-                      ].join('')
-                });
-            }
-            else {
-                city.balloon.close();
-                placemark = new ymaps.Placemark(city.getCenter(), {
-                    hintContent: 'Собственный значок метки',
-                    balloonContent: 'Это красивая метка'
-                } )
-                city.geoObjects.add(placemark);
+    })
+    clusterer = new ymaps.Clusterer({clusterDisableClickZoom:true})
+    renderGeoObjects(city);
 
-            }
-        });
+})
+}
 
+function getReviewsFromLS(){
+    const reviews = localStorage.reviews
+    return JSON.parse(reviews || '[]')
+}
+
+function getReviewList(currentGeoObjects){
+    let reviewListHTML =''
+
+    for(const review of getReviewsFromLS()){
+        if(currentGeoObjects.some(geoObject => JSON.stringify(geoObject.geometry._coordinates) === JSON.stringify(review.coords))){
+            reviewListHTML += `
+            <div class='review'>
+                <div> <strong> Место: </strong> ${review.place}</div>
+                <div> <strong> Имя: </strong> ${review.author}</div>
+                <div> <strong> Отзыв: </strong> ${review.reviewText}</div>
+            </div>    
+            `
+    }
+}
+return reviewListHTML;
+}
+
+function renderGeoObjects(city) {
+    const geoObjects = [];
+
+    for(const review of getReviewsFromLS()){
+        const placemark = new ymaps.Placemark(review.coords)
+        placemark.events.add('click', e => {
+            e.stopPropagation()
+            openBallon(city, e.get('coords'), [e.get('target')] )
+        })
+        geoObjects.push(placemark)
+    }
+    clusterer.removeAll()
+    city.geoObjects.remove(clusterer)
+    clusterer.add(geoObjects)
+    city.geoObjects.add(clusterer)
+}
+
+async function openBallon(city, coords, currentGeoObjects){
+    await city.balloon.open(coords, {
+                            contentHeader:'Отзывы:',
+                            contentBody:
+                                `<div class='reviews'>${getReviewList(currentGeoObjects)}</div>` + form
+                        });
+document.querySelector('#add-form').addEventListener('submit', function(e){
+    e.preventDefault();
+    const review = {
+        coords,
+        author: this.elements.author.value,
+        place: this.elements.place.value,
+        reviewText: this.elements.review.value,
+    }
+    localStorage.reviews = JSON.stringify([...getReviewsFromLS(), review])
+
+    renderGeoObjects(city)
+
+    city.balloon.close();
 })
 
 }
+
+
+
+
+
---
+++ src/templates/form.js
@@ -0,0 +1,8 @@
+export const form = `
+<form id='add-form'>
+<input type='text' name='author' placeholder='Укажите ваше имя'> </input><br>
+<input type 'text' name='place' placeholder='Укажите ваше место'> </input><br>
+<textarea name='review' placeholder='Укажите ваше место'></textarea><br>
+<button id='add-btn'>Добавить</button><br>
+</form>
+`;
